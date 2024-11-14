d1e37703e55e429be9b63e7d4180f53efa640b9a
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-11-14 18:51:27 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+
+lower = int(input("Enter your lowest value"))
+higher = int(input("Enter your highest value"))
+
+for i in range(lower,higher+1):
+
+    if i%2==0 :
+         
+        print(i)
+
